5b903aeff23fd0abb769249e3ed95e82740f859b
Bernhard Halbartschlager <halbart.bernhard+git@gmail.com>
2021-03-20 21:56:26 +0100

    Init admin UI

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -1,1 +1,16 @@
-<h1>Hello World!</h1>
+<?php
+//script('mohoo', 'script');
+style('mohoo', 'admin');
+?>
+
+<div id="mohoo-settings">
+    <h1 class="h1">Mohoo Settings</h1>
+
+    <form class="form">
+        <label class="text-input">
+            Mailcow API Key
+            <input type="text" name="api-key" placeholder="00000000-0000-0000-0000-000000000000"/>
+        </label>
+        <button>Save</button>
+    </form>
+</div>
